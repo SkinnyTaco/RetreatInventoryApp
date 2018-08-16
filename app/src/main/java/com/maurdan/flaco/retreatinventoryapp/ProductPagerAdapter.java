2c657467365eaa1b44500639e43dcb65d8fc2d59
Daniel Dawson <dannyldawson@gmail.com>
2018-08-16 13:01:08 -0700

    Added string resources and logic for PagerAdapter

--- app/src/main/java/com/maurdan/flaco/retreatinventoryapp/ProductPagerAdapter.java
+++ app/src/main/java/com/maurdan/flaco/retreatinventoryapp/ProductPagerAdapter.java
@@ -1,6 +1,7 @@
 package com.maurdan.flaco.retreatinventoryapp;
 
 import android.content.Context;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -21,10 +22,25 @@ public class ProductPagerAdapter extends FragmentPagerAdapter {
                 return new PetesFragment();
             case 1:
                 return new BFullerFragment();
-            case 3:
+            case 2:
                 return new CamposFragment();
             default:
                 return null;
+        }
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return mContext.getString(R.string.petes_tab_name);
+            case 1:
+                return mContext.getString(R.string.bfuller_tab_name);
+            case 2:
+                return mContext.getString(R.string.campos_tab_name);
+            default:
+                return mContext.getString(R.string.unknown_vendor_tab_name);
         }
     }
 
